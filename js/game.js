9f23f963fb8b40770426c197457968a507b984a4
Snowkedd <maxim.thomas.be@gmail.com>
2020-11-23 17:40:22 +0100

    Update - game.js
    Début des mouvements

--- js/game.js
+++ js/game.js
@@ -13,28 +13,51 @@ var config = {
     width: 900,
     height: 400,
     canvas: document.getElementById('myCustomCanvas'),
+    physics: {
+        default: 'arcade',
+            arcade: {
+                gravity: { y: 300 },
+                debug: false
+            }
+    },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 var game = new Phaser.Game(config);
+var player;
 
 function preload ()
 {
     this.load.image('tempBackground', 'assets/tempBackground.jpg');
-    this.load.image('tempPerso', 'assets/testPerso.jpg');
-    
+    this.load.image('tempPerso', 'assets/testPerso.jpg'); 
+
 }
 
 function create ()
 {
     this.add.image(config.width / 2, config.height / 2, 'tempBackground');
-    let tempPerso = this.add.image(config.width / 2, config.height / 2, 'tempPerso');
-    tempPerso.setScale(0.5,0.5);
+    player = this.physics.add.sprite(config.width / 2, config.height / 2, 'tempPerso');
+    player.setScale(0.5,0.5);
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update(){
-    
+    if(cursors.right.isDown){
+        player.setVelocityX(160);
+    }
+    else if(cursors.left.isDown){
+        player.setVelocityX(-160);
+    }
+    else if(cursors.up.isDown){
+        player.setVelocityY(-250);
+    }
+    else{
+        player.setVelocityX(0);
+    }
 }
