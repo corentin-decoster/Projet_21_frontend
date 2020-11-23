9557a1ae22af55b8c697940f92037af81f378d72
Snowkedd <maxim.thomas.be@gmail.com>
2020-11-23 15:57:11 +0100

    refactor - alll bugged fixed atm
    !!! don't forget to launch with live server !!!

--- js/game.js
+++ js/game.js
@@ -1,29 +1,3 @@
-/*
-//create a new scene
-let gameScene = new Phaser.Scene('Game');
-
-//load assets
-gameScene.preload = function(){
-  //load images
-  this.load.image('tempBackground', './assets/tempBackground.jpg');
-};
-
-gameScene.create = function(){
-  let bg = this.add.image(0,0,'tempBackground');
-  //dimension tempBackground -> 900x420
-  bg.setOrigin(900/2, 420/2);
-};
-
-//set the configuration
-var config = {
-    type: Phaser.AUTO,
-    width: 900,
-    height: 420,
-    scene: gameScene
-};
-//create a new game
-let game = new Phaser.Game(config);
-*/
 
 var myCustomCanvas = document.createElement('canvas');
 
@@ -32,13 +6,12 @@ myCustomCanvas.style = 'border: 8px solid red';
 
 document.body.appendChild(myCustomCanvas);
 
-// var myCustomContext = myCustomCanvas.getContext('2d');
+var myCustomContext = myCustomCanvas.getContext('2d');
 
 var config = {
     type: Phaser.CANVAS,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
+    width: 900,
+    height: 400,
     canvas: document.getElementById('myCustomCanvas'),
     scene: {
         preload: preload,
@@ -50,10 +23,10 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('pic', 'assets/FrozenForest_Tilese_Background.png');
+    this.load.image('tempBackground', 'assets/tempBackground.jpg');
 }
 
 function create ()
 {
-    this.add.image(400, 300, 'pic');
+    this.add.image(config.width / 2, config.height / 2, 'tempBackground');
 }
