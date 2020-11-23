b0c5d602bc451f3678f2397aa7c7fcedb8f18f86
Snowkedd <maxim.thomas.be@gmail.com>
2020-11-23 15:07:43 +0100

    add - phase 2 full tests
    pleins de test

--- js/game.js
+++ js/game.js
@@ -1,24 +1,59 @@
+/*
 //create a new scene
 let gameScene = new Phaser.Scene('Game');
 
 //load assets
 gameScene.preload = function(){
   //load images
-  this.load.image('tempBackground','../assets/tempBackground');
+  this.load.image('tempBackground', './assets/tempBackground.jpg');
 };
 
 gameScene.create = function(){
-let bg = this.add.sprite(0,0,'tempBackground');
-//dimension tempBackground -> 900x420
-bg.setOrigin(900/2, 420/2);
+  let bg = this.add.image(0,0,'tempBackground');
+  //dimension tempBackground -> 900x420
+  bg.setOrigin(900/2, 420/2);
 };
 
 //set the configuration
-let config= {
-    type: Pasher.AUTO,
-    width: 640,
-    height: 360,
+var config = {
+    type: Phaser.AUTO,
+    width: 900,
+    height: 420,
     scene: gameScene
 };
 //create a new game
 let game = new Phaser.Game(config);
+*/
+
+var myCustomCanvas = document.createElement('canvas');
+
+myCustomCanvas.id = 'myCustomCanvas';
+myCustomCanvas.style = 'border: 8px solid red';
+
+document.body.appendChild(myCustomCanvas);
+
+// var myCustomContext = myCustomCanvas.getContext('2d');
+
+var config = {
+    type: Phaser.CANVAS,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    canvas: document.getElementById('myCustomCanvas'),
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('pic', 'assets/FrozenForest_Tilese_Background.png');
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'pic');
+}
