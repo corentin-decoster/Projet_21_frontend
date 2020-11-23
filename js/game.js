3808fef9744bf14f667ca4ce4cd75196ba741956
Snowkedd <maxim.thomas.be@gmail.com>
2020-11-23 19:08:26 +0100

    Fin de mouvement-personnage (pas de script)
    Script possible à rajouter très facilement, il suffit d'avoir la bonne spritesheet !

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 var myCustomCanvas = document.createElement('canvas');
 
 myCustomCanvas.id = 'myCustomCanvas';
-myCustomCanvas.style = 'border: 8px solid red';
+myCustomCanvas.style = 'border: 8px black';
 
 document.body.appendChild(myCustomCanvas);
 
@@ -29,35 +29,56 @@ var config = {
 
 var game = new Phaser.Game(config);
 var player;
+var cursors;
 
 function preload ()
 {
     this.load.image('tempBackground', 'assets/tempBackground.jpg');
-    this.load.image('tempPerso', 'assets/testPerso.jpg'); 
-
+    this.load.spritesheet('tempPerso', 'assets/Skinless Yarik/body/firstcharacter.png', { frameWidth: 34, frameHeight: 34 }); 
 }
 
 function create ()
 {
     this.add.image(config.width / 2, config.height / 2, 'tempBackground');
     player = this.physics.add.sprite(config.width / 2, config.height / 2, 'tempPerso');
-    player.setScale(0.5,0.5);
+
+    player.setScale(2,2);
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
+
     cursors = this.input.keyboard.createCursorKeys();
+
+    this.anims.create({
+        key: 'left',
+        frames: [ { key: 'tempPerso', frame: 0 } ],
+        frameRate: 1
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: [ { key: 'tempPerso', frame: 1 } ],
+        frameRate: 1
+    });
+
+    console.log(this);
 }
 
-function update(){
+function update()
+{
     if(cursors.right.isDown){
         player.setVelocityX(160);
+        player.anims.play('right');
     }
     else if(cursors.left.isDown){
         player.setVelocityX(-160);
-    }
-    else if(cursors.up.isDown){
-        player.setVelocityY(-250);
+        player.anims.play('left');
+        
     }
     else{
         player.setVelocityX(0);
+    }
+
+    if(cursors.up.isDown && player.body.blocked.down){
+        player.setVelocityY(-250);
     }
 }
