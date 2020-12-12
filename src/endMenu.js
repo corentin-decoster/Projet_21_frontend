46e98affa47cec5858ed3317b99ed695f02c16c2
axel <axel.declerck@student.vinci.be>
2020-12-12 19:02:47 +0100

    Update endMenu.js

--- src/endMenu.js
+++ src/endMenu.js
@@ -23,7 +23,40 @@ class EndMenu extends Phaser.Scene {
         this.add.image(370,150,'score');
         this.add.text(400,280,`MY SCORE HUEHUE IS : ${this.playedTime}`);
         var element = this.add.dom(400, 250).createFromCache('form');
-        
+        element.addListener('click');
+
+        element.on('click', function (event) {
+    
+            if (event.target.name === 'playButton')
+            {
+                var inputText = this.getChildByName('nameField');
+    
+                //  Have they entered anything?
+                if (inputText.value !== '')
+                {
+                    //  Turn off the click events
+                    this.removeListener('click');
+    
+                    //  Hide the login element
+                    this.setVisible(false);
+    
+                    //  Populate the text with whatever they typed in
+                    text.setText('Welcome ' + inputText.value);
+                }
+                else
+                {
+                    //  Flash the prompt
+                    this.scene.tweens.add({
+                        targets: text,
+                        alpha: 0.2,
+                        duration: 250,
+                        ease: 'Power3',
+                        yoyo: true
+                    });
+                            }
+            }
+    
+        });
 
        
    }
