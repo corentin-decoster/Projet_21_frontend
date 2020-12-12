7856eca895ff373fd1a7a5327c3510df699f16a2
axel <axel.declerck@student.vinci.be>
2020-12-12 19:01:37 +0100

    score

--- src/endMenu.js
+++ src/endMenu.js
@@ -15,12 +15,14 @@ class EndMenu extends Phaser.Scene {
 
     preload(){
         this.load.image('score','./assets/score.png');
+        this.load.html('form', './form.html');
     }
 
 
     create() {
         this.add.image(370,150,'score');
         this.add.text(400,280,`MY SCORE HUEHUE IS : ${this.playedTime}`);
+        var element = this.add.dom(400, 250).createFromCache('form');
         
 
        
